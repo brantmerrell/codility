db51e714781c9e62046af5a7f0ac7ba4ac05546f
Ubuntu <ubuntu@ip-172-31-53-197.ec2.internal>
2018-02-15 20:16:02 +0000

    performance revision PermMissingElem

--- 3.1_PermMissingElem.js
+++ 3.1_PermMissingElem.js
@@ -14,16 +14,17 @@ function PermMissingElem(A){
 		return(1)
 	
  	// if no skips exist in set (and it begins at 1),
-	}else if(Math.max.apply(Math,A)==A.length){
+	}
+	if(Math.max.apply(Math,A)==A.length){
 
 	// missing element is at end of set
-		return(A.length+1) 
+		return(A.length+1)
 	
 	// otherwise missing element is within set
 	}else{
 
 		// therefore, first sort the set
-		A = A.sort()
+		A = A.sort(function(a,b){return(a-b)})
 
 		// then crawl along array and test each element
 		//for n in range(0, N):
@@ -39,16 +40,26 @@ function PermMissingElem(A){
 	}
 }
 // Console Testing:
-console.log(["Empty list - []; expect 1: "]+[PermMissingElem([])])
-console.log(["Single element - [1]; expect 2: "]+[PermMissingElem([1])])
-console.log(["Missing first - [3,2,5,4]; expect 1: "]+[PermMissingElem([3,2,5,4])])
-console.log(["Missing last - [1,2,4,3]; expect 5: "]+[PermMissingElem([1,2,4,3])])
-console.log(["Double - [1,3]; expect 2: "]+[PermMissingElem([1,3])])
-console.log(["Practice - [2,3,1,5]; expect 4: "]+[PermMissingElem([2,3,1,5])])
+//console.log(["Empty list - []; expect 1: "]+[PermMissingElem([])])
+//console.log(["Single element - [1]; expect 2: "]+[PermMissingElem([1])])
+//console.log(["Missing first - [3,2,5,4]; expect 1: "]+[PermMissingElem([3,2,5,4])])
+//console.log(["Missing last - [1,2,4,3]; expect 5: "]+[PermMissingElem([1,2,4,3])])
+//console.log(["Double - [1,3]; expect 2: "]+[PermMissingElem([1,3])])
+//console.log(["Practice - [2,3,1,5]; expect 4: "]+[PermMissingElem([2,3,1,5])])
+/// Performance testing:
+var myArray = []
+for(i=0; i<10000; i++){
+	myArray.push(i+1);
+}
+var SPLICE = Math.floor(Math.random(myArray.length)*myArray.length)
+console.log(["Expect: "]+ [myArray[SPLICE]]+["; length:"]+[myArray.length]+["; max: "]
+	+[Math.max.apply(Math,myArray)]+["; min: "]+[Math.min.apply(Math,myArray)])
+myArray.splice(SPLICE,1)
+console.log(["length = "]+[myArray.length]+[myArray.length]+["; max: "]
+	+[Math.max.apply(Math,myArray)]+["; result = "]+[PermMissingElem(myArray)])
 
 // Codility testing: 
 /// https://app.codility.com/programmers/lessons/3-time-complexity/perm_missing_elem/
-/// accuracy = 
-/// performance = 
-
+/// correctness = 100% 
+/// performance = 100%
 
--- 3.2_TapeEquilibrium.js
+++ 3.2_TapeEquilibrium.js
@@ -8,12 +8,12 @@ function TapeEquilibrium(A){
 
 	// splice A[0] from A, store their difference in array "Diff"
 	var sum1 = A[0]
-	var sum2 = A.reduce(add,0);function add(a,b){return(a+b)}
-	sum2 = sum2 - A[0]
+	function add(a,b){return(a+b)}
+	var sum2 = A.reduce(add,0)-A[0];
 	Diff = [Math.abs(sum1-sum2)]
 
 	// build "Diff" array
-	for(n=1; n<=A.length; n++){
+	for(n=1; n<A.length-1; n++){
 		sum1 = sum1 + A[n]
 		sum2 = sum2 - A[n]
 		Diff.push(Math.abs(sum1-sum2))
@@ -23,9 +23,11 @@ function TapeEquilibrium(A){
 }
 // console testing:
 console.log(["TapeEquilibrium([3,1,2,4,3]); expect 1: "]+[TapeEquilibrium([3,1,2,4,3])])
+console.log(["TapeEquilibrium([-1000,1000]); expect 2000: "]+[TapeEquilibrium([-1000,1000])])
+console.log(["TapeEquilibrium([10,-10]); expect 20: "]+[TapeEquilibrium([10,-10])])
 
 // Codility testing:
 /// https://app.codility.com/programmers/lessons/3-time-complexity/tape_equilibrium/
-/// performance:
-/// correctness:
+/// performance: 100%
+/// correctness: 100%
 
