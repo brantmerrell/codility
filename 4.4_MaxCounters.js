182889acbc900ccd715ab8d294df8117823cbd9b
Joshua Brant <brant.merrell@gmail.com>
2018-02-22 22:05:35 +0000

    fix MaxCounters

--- 4.4_MaxCounters.js
+++ 4.4_MaxCounters.js
@@ -12,32 +12,25 @@ function MaxCounters(N, A) {
     }
     var B = 0; // define variable to track 
     var C = 0; // define variable to track
-    for (i = 0; i < A.length; i++) {
+    for (var i = 0; i < A.length; i++) {
         if (1 <= A[i] && A[i] <= N) {
             if (newArray[A[i] - 1] < B) {
-		console.log("newArray[A[i]("+A[i]+") - 1]("+newArray[A[i]-1]+") < B"+B+"->newArray[A[i]-1]=B")
                 newArray[A[i] - 1] = B;
-		console.log("newArray = "+newArray)
             }
             newArray[A[i] - 1] = newArray[A[i] - 1] + 1;
             if (C < newArray[A[i] - 1]) {
-		console.log("C < newArray[A[i] - 1]  ->  C=newArray[A[i]-1] = "+ newArray[A[i]-1])
-		C = newArray[A[i] - 1];
+                C = newArray[A[i] - 1];
             }
         }
         if (A[i] == N + 1) {
             B = C;
         }
     }
-    for (i = 0; i <= N; i++) {
+    for (var i = 0; i <= N; i++) {
         if (newArray[i] < B) {
             newArray[i] = B;
         }
     }
-//    function isNumber(obj){
-//	return(obj!==undefined && typeof obj === 'number' && !isNaN(obj))
-//    }
-//    newArray = newArray.filter(isNumber)
     return (newArray);
 }
 // console testing:
@@ -45,6 +38,6 @@ console.log("MaxCounters(5,[3,4,4,6,1,4,4]); expect 3,2,2,4,2: " + MaxCounters(5
 console.log("MaxCounters(1,[1]); expect _: " + MaxCounters(1, [1]));
 // codility testing:
 /// https://app.codility.com/programmers/lessons/4-counting_elements/max_counters/
-/// Correctness = 75% (got  expect one) 
-/// Performance = 80% (got array of zeros with wrong length)
+/// Correctness = 100% 
+/// Performance = 100% 
 /// Difficulty: Respectable
--- 4.4_MaxCounters.ts
+++ 4.4_MaxCounters.ts
@@ -1,47 +1,43 @@
 // builds array of length N based on inputs from array A
 /// adds 1 to newArrayAk when 1<= Ak <= N
 /// sets all newArray values to max(newArray) when Ak = N+1
-
 // Assumptions
 /// 1 <= N <= 100,000
 /// 1 <= M <= 100,000
 /// 1 <= Ai < N
-
-function MaxCounters(N,A){
-	var newArray = [0]
-	while(newArray.length<N){newArray.push(0)}
-
-	var B = 0 // define variable to track 
-	var C = 0 // define variable to track
-	for(var i=0; i<=A.length; i++){
-		if(1 <= A[i] <= N){
-			if(newArray[A[i-1]] < B){
-				newArray[A[i-1]] = B
-			}
-			newArray[A[i-1]] = newArray[A[i-1]]+1
-			if(C < newArray[A[i-1]]){
-				C = newArray[A[i-1]]
-			}
-		}
-		if(A[i]==N+1){
-			B = C
-		}
-	}
-	for(var i=0; i<=N; i++){
-		if(newArray[i] < B){
-			newArray[i] = B
-		}
-	}
-	newArray.pop()
-	return(newArray)
+function MaxCounters(N, A) {
+    var newArray = [0];
+    while (newArray.length < N) {
+        newArray.push(0);
+    }
+    var B = 0; // define variable to track 
+    var C = 0; // define variable to track
+    for (var i = 0; i < A.length; i++) {
+        if (1 <= A[i] && A[i] <= N) { // abbreviating to 1<=A[i]<=N makes debugging a nightmare
+            if (newArray[A[i] - 1] < B) {
+                newArray[A[i] - 1] = B;
+            }
+            newArray[A[i] - 1] = newArray[A[i] - 1] + 1;
+            if (C < newArray[A[i] - 1]) {
+		C = newArray[A[i] - 1];
+            }
+        }
+        if (A[i] == N + 1) {
+            B = C;
+        }
+    }
+    for (var i = 0; i <= N; i++) {
+        if (newArray[i] < B) {
+            newArray[i] = B;
+        }
+    }
+    return (newArray);
 }
-
 // console testing:
-console.log("MaxCounters(5,[3,4,4,6,1,4,4]); expect 3,2,2,4,2: "+MaxCounters(5,[3,4,4,6,1,4,4]))
-console.log("MaxCounters(1,[1]); expect _: "+MaxCounters(1,[1]))
-
+console.log("MaxCounters(5,[3,4,4,6,1,4,4]); expect 3,2,2,4,2: " + MaxCounters(5, [3, 4, 4, 6, 1, 4, 4]));
+console.log("MaxCounters(1,[1]); expect _: " + MaxCounters(1, [1]));
 // codility testing:
 /// https://app.codility.com/programmers/lessons/4-counting_elements/max_counters/
-/// Correctness = 75% (got  expect one) 
-/// Performance = 80% (got array of zeros with wrong length)
+/// Correctness = 100% 
+/// Performance = 100% 
 /// Difficulty: Respectable
