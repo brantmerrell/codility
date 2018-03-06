de97e3065eb81a1d61298f698603d281692b12d4
Joshua Brant Merrell <brant.merrell@gmail.com>
2018-03-05 19:15:11 -0500

    prefix sums

--- 3/lesson.py
+++ 3/lesson.py
@@ -2,29 +2,76 @@
 
 def prefix_sums(arr_a):
     """5.1 Counting prefix sums"""
+    # define length
     len_n = len(arr_a)
+    # array result (arr_res) should have length len_n+1
+    # (its first and last elements should respectively be the sum of none & of all)
     arr_res = [0] * (len_n + 1)
+    # iterate from 2nd index (1) to last (len_n+1)
     for k in range(1, len_n + 1):
+        # add the most recent sum to arr_a's corresponding index
+        # (arr_a's corresponding index is [k - 1])
         arr_res[k] = arr_res[k - 1] + arr_a[k - 1]
+    # return array result
     return arr_res
 
 def count_total(arr_p, int_x, int_y):
     """5.2 total of one slice"""
-    return arr_p[int_y + 1] - arr_p[int_x]
-
-def mushrooms(arr_a, int_k, int_m):
-    """5.3 mushroom picker O(n+m)"""
-    int_n = len(arr_a)
-    result = 0
-    arr_pref = prefix_sums(arr_a)
-    for ndx_p in range(min(int_m, int_k) + 1):
-        left_pos = int_k - ndx_p
-        right_pos = min(int_n - 1, max(int_k, int_k + int_m - 2 * ndx_p))
-        result = max(result, count_total(arr_pref, left_pos, right_pos))
-        print({"loop":1, "A[p]":arr_a[ndx_p], "left":left_pos, "right":right_pos, "result":result})
-    for ndx_p in range(min(int_m + 1, int_n - int_k)):
-        right_pos = int_k + ndx_p
-        left_pos = max(0, min(int_k, int_k - (int_m - 2 * ndx_p)))
-        result = max(result, count_total(arr_pref, left_pos, right_pos))
-        print({"loop":2,'ndx_p':ndx_p, "A[p]":arr_a[ndx_p], "left":left_pos, "right":right_pos, "result":result})
+    # because arr_p was built as a sequential sum from arr_a,
+    # the sum of a range in arr_a == the difference between two points of arr_p
+    result = arr_p[int_y + 1] - arr_p[int_x]
     return result
+
+def mushrooms(arr_a, pos_k, mov_m):
+    """5.3 mushroom picker O(n+m)"""
+    # store length of input array
+    len_n = len(arr_a)
+    # define result as lowest theoretical possibility
+    result = 0
+    # generate array of prefix sums
+    arr_pref = prefix_sums(arr_a)
+    # LOOP 1: TRAVELLING LEFT THEN RIGHT
+    # define the maximum leftwards distance that can be travelled from pos_k
+    # max leftwards distance is limited by both the distance limit (mov_m)
+    # and by the number of elements that exist to the left of pos_k.
+    index_max = min(mov_m, pos_k)
+    # pos_k - index_max must be the 1st index, which is zero. So add 1 to index_max
+    index_max += 1
+    # iterate from 0 to index_max
+    for ndx_p in range(index_max):
+        # define index of left boundary of range as distance ndx_p to the left of pos_k
+        pos_left = pos_k - ndx_p
+        # define leftover allowed distance after travelling
+        # from pos_k to pos_left and back (mov_m - 2 * ndx_p),
+        # and add to pos_k to obtain rightwards range boundary
+        pos_right = pos_k + mov_m - 2 * ndx_p
+        # if this puts pos_right to the left of starting point, define as starting point
+        pos_right = max(pos_right, pos_k)
+        # if pos_right is outside array boundary, redefine as last index
+        pos_right = min(pos_right, len_n - 1)
+        # call count_total to generate the sum of elements within travel range
+        # if the output of count_total is higher than result, redefine result
+        result = max(result, count_total(arr_pref, pos_left, pos_right))
+    # LOOP 2: TRAVELLING RIGHT THEN LEFT
+    # store the maximum rightwards distance from pos_k through which to iterate
+    # rightwards distance is limited by allowed travel distance (mov_m + 1),
+    # as well as the distance from pos_k to the end of the array
+    index_max = min(mov_m + 1, len_n - pos_k)
+    # iterate from 0 to maximum rightwards distance
+    for ndx_p in range(index_max):
+        # define rightwards range boundary as ndx_p indices to the right of pos_k
+        pos_right = pos_k + ndx_p
+        # define leftover allowed distance after travelling
+        # from pos_k to pos_right and back (mov_m - 2 * ndx_p),
+        # and subtract from pos_k to obtain leftwards range boundary
+        pos_left = pos_k - (mov_m - 2 * ndx_p)
+        # if this puts pos_left to the right of starting point, define as starting point
+        pos_left = min(pos_left, pos_k)
+        # if pos_left is lower than zero, define as zero
+        pos_left = max(pos_left, 0)
+        # call count_total to generate the sum of elements within travel range
+        # if the output of count_total is higher than result, redefine result
+        result = max(result, count_total(arr_pref, pos_left, pos_right))
+    # return result
+    return result
+RESULT = mushrooms([2, 3, 7, 5, 1, 3, 9], 4, 6)
--- 3/lesson.ts
+++ 3/lesson.ts
@@ -1,30 +1,32 @@
 // https://codility.com/media/train/3-PrefixSums.pdf
-function prefix_sums(arr_a) {
+
+function prefix_sums(arr_a){
     // 5.1 Counting prefix sums
     // define length
     var len_n = arr_a.length;
     // array result (arr_res) should have length len_n+1
     // (its first and last elements should respectively be the sum of none & of all)
     var arr_res = [0];
-    while (arr_res.length < len_n) {
+    while(arr_res.length < len_n){
         arr_res.push(0);
     }
     // iterate from 2nd index (1) to last (len_n+1)
-    for (var k = 1; k < len_n + 1; k++) {
+    for(var k=1; k<len_n+1; k++){
         // add the most recent sum to arr_a's corresponding index
         // (arr_a's corresponding index is [k - 1])
         arr_res[k] = arr_res[k - 1] + arr_a[k - 1];
     }
-    return (arr_res);
+
+    return(arr_res);
 }
-function count_total(arr_p, ndx_x, ndx_y) {
+function count_total(arr_p, ndx_x, ndx_y){
     // 5.2 total of one slice
     // because arr_p was built as a sequential sum from arr_a,
     // the sum of a range in arr_a == the difference between two points of arr_p
     var result = arr_p[ndx_y + 1] - arr_p[ndx_x];
-    return (result);
+    return(result);
 }
-function mushrooms(arr_a, pos_k, mov_m) {
+function mushrooms(arr_a, pos_k, mov_m){
     // 5.3 mushroom picker O(n+m)
     // store length of input array
     var len_n = arr_a.length;
@@ -40,7 +42,7 @@ function mushrooms(arr_a, pos_k, mov_m) {
     // pos_k - index_max must be the 1st index, which is zero. So add 1 to index_max
     indexMax += 1;
     //  iterate from 0 to index_max
-    for (var ndx_p = 0; ndx_p < indexMax; ndx_p++) {
+    for(var ndx_p=0; ndx_p<indexMax; ndx_p++){
         // define index of left boundary of range as distance ndx_p to the left of pos_k
         var pos_left = pos_k - ndx_p;
         // define leftover allowed distance after travelling
@@ -61,7 +63,7 @@ function mushrooms(arr_a, pos_k, mov_m) {
     // as well as the distance from pos_k to the end of the array
     indexMax = Math.min(mov_m + 1, len_n - pos_k);
     // iterate from 0 to maximum rightwards distance
-    for (ndx_p = 0; ndx_p < indexMax; ndx_p++) {
+    for(ndx_p=0; ndx_p<indexMax; ndx_p++){
         // define rightwards range boundary as ndx_p indices to the right of pos_k
         pos_right = pos_k + ndx_p;
         // define leftover allowed distance after travelling
@@ -77,7 +79,7 @@ function mushrooms(arr_a, pos_k, mov_m) {
         result = Math.max(result, count_total(arr_pref, pos_left, pos_right));
     }
     // return result
-    return (result);
+    return(result);
 }
 // eslint-disable-next-line no-unused-vars
-var result = mushrooms([2, 3, 7, 5, 1, 3, 9], 4, 6);
+var result = mushrooms([2,3,7,5,1,3,9], 4, 6);
