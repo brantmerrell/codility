1def1849c9a6502b298a50b6ac81cdb49bbbeb5d
Debian <admin@ip-172-31-43-75.us-east-2.compute.internal>
2021-05-29 19:27:33 -0500

    fix cyclic rotation (py)

--- source/2-arrays/2/cyclic_rotation.py
+++ source/2-arrays/2/cyclic_rotation.py
@@ -3,14 +3,14 @@
 ## 1 <= N <= 100
 ## 1 <= K <= 100
 ## -1,000 <= A[i] <= 1,000
-def CyclicRotation(A, K){
-	if 0 < len(A):
-		N = 0
-		while N < K:
-			A = [A[len(A)-1]]+A[0:(len(A)-1)]
-		N = N + 1
-	return(A)
-}
+def CyclicRotation(A, K):
+    if len(A)==0 or K == len(A) or K == 0:
+        return A
+    while K > len(A):
+        K = K - len(A)
+    M = len(A) - K
+    return A[M:] + A[:M]
+
 # console testing
 print("CyclicRotation([3,8,9,7,6], 3); expect [9,7,6,3,8]: "+CyclicRotation([3,8,9,7,6], 3))
 print("CyclicRotation([1,2,3,4], 4); expect [1,2,3,4]: "+CyclicRotation([1,2,3,4], 4))
